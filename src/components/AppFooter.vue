4aa0cafa94f0b5fa82f4586ec1a5e479b12d7361
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-23 18:13:41 +0100

    add: footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,15 +2,241 @@
 export default {
     data() {
         return {
-            
+            footerTopLinks: [
+                {
+                    title: "dc comics",
+                    subtitle: [
+                        {
+                            link: "Characters",
+                        },
+                        {
+                            link: "Comics",
+                        },
+                        {
+                            link: "Movies",
+                        },
+                        {
+                            link: "TV",
+                        },
+                        {
+                            link: "Games",
+                        },
+                        {
+                            link: "Videos",
+                        },
+                        {
+                            link: "News",
+                        },
+                    ],
+
+                },
+                {
+                    title: "dc",
+                    subtitle: [
+                        {
+                            link: "Terms Of Use",
+                        },
+                        {
+                            link: "Privacy Policy (New)",
+                        },
+                        {
+                            link: "Ad Choices",
+                        },
+                        {
+                            link: "Advertising",
+                        },
+                        {
+                            link: "Jobs",
+                        },
+                        {
+                            link: "Subscriptions",
+                        },
+                        {
+                            link: "Talent Workshops",
+                        },
+                        {
+                            link: "CPSC Certidicates",
+                        },
+                        {
+                            link: "Ratings",
+                        },
+                        {
+                            link: "Shop Help",
+                        },
+                        {
+                            link: "Contact Us",
+                        },
+                    ],
+
+                },
+                {
+                    title: "sites",
+                    subtitle: [
+                        {
+                            link: "DC",
+                        },
+                        {
+                            link: "MAD Magazine",
+                        },
+                        {
+                            link: "DC Kids",
+                        },
+                        {
+                            link: "DC Universe",
+                        },
+                        {
+                            link: "DC Power Visa",
+                        },
+                    ]
+                },
+                {
+                    title: "shop",
+                    subtitle: [
+                        {
+                            link: "Shop DC",
+                        },
+                        {
+                            link: "Shop DC Collectibles"
+                        },
+                    ],
+                },
+            ],
+            footerBotLinks: [
+                {
+                    link: "facebook"
+                },
+                {
+                    link: "periscope"
+                },
+                {
+                    link: "pinterest"
+                },
+                {
+                    link: "twitter"
+                },
+                {
+                    link: "youtube"
+                }
+            ]
+        }
+    },
+    methods: {
+        getImagePath(imgName) {
+            return new URL(`../assets/img/footer-${imgName}.png`, import.meta.url).href;
         }
     }
 }
 </script>
 
 <template>
-<h2>Footer</h2>
+    <footer>
+        <div class="container">
+            <section class="footer-top">
+                <div class="row">
+                    <div class="col">
+                        <h4>{{ footerTopLinks[0].title }}</h4>
+                        <ul class="mb-1">
+                            <li v-for="link in footerTopLinks[0].subtitle">{{ link.link }}</li>
+                        </ul>
+                        <h4>{{ footerTopLinks[3].title }}</h4>
+                        <ul>
+                            <li v-for="link in footerTopLinks[3].subtitle">{{ link.link }}</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h4>{{ footerTopLinks[1].title }}</h4>
+                        <ul>
+                            <li v-for="link in footerTopLinks[1].subtitle">{{ link.link }}</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h4>{{ footerTopLinks[2].title }}</h4>
+                        <ul>
+                            <li v-for="link in footerTopLinks[2].subtitle">{{ link.link }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <section class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <button>SIGN-UP NOW!</button>
+                    <ul>
+                        <li>
+                            <h4>FOLLOW US</h4>
+                        </li>
+                        <li v-for="img in footerBotLinks"><a href=""><img :src="getImagePath(img.link)" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
+footer {
+    background-image: url(../assets/img/footer-bg.jpg);
+
+    .footer-top {
+        background-image: url(../assets/img/dc-logo-bg.png);
+        background-repeat: no-repeat;
+        background-position: right;
+
+        .row {
+            padding: 2rem 0;
+            @include flex(row, space-between, stretch);
+            width: 42%;
+
+            .col {
+                width: 32%;
+
+                h4 {
+                    text-transform: uppercase;
+                    color: white;
+                    margin-bottom: .5rem;
+                }
+
+                li {
+                    padding: .2rem 0;
+                    font-size: 12px;
+                    color: rgb(142 144 146);
+                }
+
+                .mb-1 {
+                    margin-bottom: .5rem;
+                }
+            }
+        }
+
+    }
+
+    .footer-bottom {
+        background-color: $footer_bg_color;
+
+        .row {
+            @include flex (row, space-between, center);
+            padding: 1.5rem 0;
+
+            button {
+                background-color: transparent;
+                padding: .5rem;
+                border: 1px solid $primary_color;
+                color: white;
+            }
+
+            ul {
+                @include flex (row, space-between, center);
+                gap: 1rem;
+
+                h4 {
+                    color: $primary_color;
+                }
+            }
+        }
+    }
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -40,20 +40,20 @@ export default {
 </script>
 
 <template>
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-1">
-                <img src="../assets/img/dc-logo.png" alt="">
-            </div>
-            <div class="col-2">
-                <ul>
-                    <li v-for="(item, index) in navItems" :key="item.title"><a href="">{{ item.title }}</a></li>
-                </ul>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </div>
+                <div class="col-2">
+                    <ul>
+                        <li v-for="(item, index) in navItems" :key="item.title"><a href="">{{ item.title }}</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
 </template>
 
 <style lang="scss" scoped>
@@ -61,24 +61,26 @@ export default {
 
 .row {
     @include flex (row, space-between, center);
-    padding: 2rem;
+    padding: 1.5rem 0;
 
     .col-1 {
-        width: 25%;
-    }
-    .col-2 {
-        width: 65%;
-        ul {
-            @include flex (row, space-between, center);
-            gap: 1rem;
-            list-style: none;
-            li a {
-                text-decoration: none;
-                text-transform: uppercase;
-                color: black;
-                font-weight: bold
-            }
+        width: fit-content;
+
+        img {
+            max-width: 80px;
         }
     }
+
+    ul {
+        @include flex (row, space-between, center);
+        gap: 1rem;
+
+        li a {
+            text-transform: uppercase;
+            color: black;
+            font-weight: bold
+        }
+    }
+
 }
 </style>
