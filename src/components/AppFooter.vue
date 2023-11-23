c62ae519218d4b2750a77811e6298f382f8ec2cd
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-23 15:29:39 +0100

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+<h2>Footer</h2>
+</template>
+
+<style lang="scss" scoped>
+</style>
